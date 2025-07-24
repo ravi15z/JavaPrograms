7ac2f24f32eff87cf31e53ba630f1ce7a2bfe0b5
ravi15z <rv.gaikwad15@gmail.com>
2025-07-24 15:47:54 +0530

    stream1

--- src/main/java/com/programs/javaprograms/JavaprogramsApplication.java
+++ src/main/java/com/programs/javaprograms/JavaprogramsApplication.java
@@ -3,12 +3,19 @@ package com.programs.javaprograms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.programs.javaprograms.java8.StreamApi;
+
 @SpringBootApplication
 public class JavaprogramsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaprogramsApplication.class, args);
 		System.out.println("HELLO");
+		streamPrograms();
 	}
 
+	private static void streamPrograms() {
+		String highestLength = StreamApi.highestLength();
+		System.out.println("Highest length word is:" + highestLength);
+	}
 }
--- src/main/java/com/programs/javaprograms/java8/StreamApi.java
+++ src/main/java/com/programs/javaprograms/java8/StreamApi.java
@@ -1,5 +1,16 @@
 package com.programs.javaprograms.java8;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class StreamApi {
 
+    public static String highestLength() {
+        String sentence = "I am learning Streams Api in Java";
+        System.out.println("find the word that has the highest lenth");
+        // we can use sorted().skip(0).findfirst() it will sort alphabatically.
+        return Arrays.stream(sentence.split(" "))
+                .max(Comparator.comparing(s -> s.length()))
+                .get();
+    }
 }
