b110e9584cbb4653d3d0bd1ccdd88be2c612bbc6
ravi15z <rv.gaikwad15@gmail.com>
2025-08-01 16:28:55 +0530

    occurance of char.

--- src/main/java/com/programs/javaprograms/stringPr/String2CharPr.java
+++ src/main/java/com/programs/javaprograms/stringPr/String2CharPr.java
@@ -1,10 +1,41 @@
 package com.programs.javaprograms.stringPr;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class String2CharPr {
 
     public void convertStringToChar() {
         String str1 = "asdfghjkl";
         char[] char1 = str1.toCharArray();
         System.out.println("String converted to char Array: " + char1);
+
+        System.out.println("char[] doesn't override toString(), so printing it directly prints the object reference.\n"
+                + "Use Arrays.toString() to print the array as a list of characters.\n"
+                + "Or use new String(char[]) if you want to treat the array as a string again.");
+        System.out.println("String converted to char Array: " + Arrays.toString(char1));
+
+        System.out.println("\n-------------------------");
+        getOccuranceOfChar();
+        System.out.println("\n-------------------------");
+
+    }
+
+    private void getOccuranceOfChar() {
+        System.out.println("\n count the frequency of characters in a string");
+        String str2 = "aabbbccccddeee";
+        System.out.println("String is: " + str2);
+        char[] char2 = str2.toCharArray();
+        Map<Character, Integer> map1 = new HashMap<>();
+        for (char c : char2) {
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Character, Integer> m : map1.entrySet()) {
+            sb.append(m.getKey()).append(m.getValue());
+        }
+        System.out.println("String : Char[] : map:" + map1);
+        System.out.println("Frequency of each character: " + sb.toString());
     }
 }
